fc1fe39b083b7deb6e417345fb94e64b9064df53
Email 6 <loserkid0006@outlook.com>
2023-04-05 14:29:57 -0600

    new stuff

--- enemy.h
+++ enemy.h
@@ -5,16 +5,18 @@ class Enemy : public GameObject {
 public:
 	Enemy(sf::Vector2f position, sf::Color color, std::string type) :
 		GameObject(position.x, position.y, tileSize, color, type) {};
-	virtual void update(std::vector<std::unique_ptr<GameObject>>& gameObjects, float delta);
-	virtual void playerCollide(std::vector<std::unique_ptr<GameObject>>& gameObjects);
+	virtual void update(std::vector<std::unique_ptr<GameObject>>& gameObjects, float delta) = 0;
+	virtual void playerCollide(std::vector<std::unique_ptr<GameObject>>& gameObjects) = 0;
 	int speed;
 	sf::Vector2f spawnPoint;
 private:
 };
 
 class Car : public Enemy {
+public:
 	Car(sf::Vector2f position, int speed) : Enemy(position, sf::Color::Red, "Car")
 		{ this->speed = speed; spawnPoint = position; };
 	void update(std::vector<std::unique_ptr<GameObject>>& gameObjects, float delta);
 	void playerCollide(std::vector<std::unique_ptr<GameObject>>& gameObjects);
+private:
 };
